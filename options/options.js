cc6292a06f2157c17a30db9575e7204dc86e351b
Crowdbotics <team@crowdbotics.com>
2021-08-12 23:06:54 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "square_leaf_29771Identifier",
+  url: "https://square_leaf_29771Identifier.botics.co",
+  api: "https://square_leaf_29771Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
